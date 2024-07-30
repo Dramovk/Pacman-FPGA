5fb4cb92b7c3b3abbe9b01d51f6035840f5ab957
Dramovk <97710294+Dramovk@users.noreply.github.com>
2024-07-30 11:45:35 -0700

    Add files via upload

---
+++ Joystick/score_controller.vhd
@@ -0,0 +1,80 @@
+--This is the primary driver that controls the displayed score.
+--Includes component calls to secondary driver which controls place values,
+--as well as the seven segment display driver to display score.
+--Rename file to **score_controller**
+-- Company: Point Loma nazarene University
+-- Engineer: Kyle Dramov
+-- 
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity score_controller is
+    Port ( score_button : in STD_LOGIC;
+            clk : in STD_LOGIC;
+            rst : in STD_LOGIC;
+            --Anodes of 7seg Display
+            score_anode : out std_logic_vector (3 downto 0):= "1111";
+            --Cathodes of 7 seg display
+            score_segment : out std_logic_vector (7 downto 0);
+            score_int : in INTEGER
+            );
+end score_controller;
+
+architecture Behavioral of score_controller is
+
+--declare secondary driver component
+component place_value_driver is
+    Port ( button : in STD_LOGIC;
+           value : out integer range 0 to 9;
+           next_digit: out STD_LOGIC);
+end component;
+
+--declare seven segment controller component
+component sev_seg_driver is
+  port (
+  data_0 : in integer range 0 to 9;
+  data_1 : in integer range 0 to 9;
+  data_2 : in integer range 0 to 9;
+  data_3 : in integer range 0 to 9;
+  clk : in STD_LOGIC;
+  rst : in STD_LOGIC:='0';
+  display_data : out STD_LOGIC_VECTOR(7 downto 0);
+  display_digit : out STD_LOGIC_VECTOR(3 downto 0));
+end component;
+
+--declare internal signals
+signal nD0: std_logic:='0'; --change variable names to enhance readibility
+signal nD1: std_logic:='0';
+signal nD2: std_logic:='0';
+signal nD3: std_logic:='0';
+
+signal ones: integer range 0 to 9;
+signal tens: integer range 0 to 9;
+signal hundreds: integer range 0 to 9;
+signal thousands: integer range 0 to 9;
+
+--score int sginals
+signal score_int_i : integer;
+--signal score_driver : std_logic := '0';
+
+begin
+Score_int_i <= score_int;
+
+process 
+begin
+    wait on score_int;
+        thousands<= (score_int_i) /1000;
+        hundreds<= (score_int_i - (thousands*1000)) /100;
+        tens<= (score_int_i - (thousands*1000) - (hundreds*100))/10;
+        ones<= (score_int_i - (thousands*1000) - (hundreds*100) - (tens*10));
+end process;
+
+    --drive internal signals with component calls for each place value
+--    dig_0: place_value_driver port map(button => score_driver, value => hex_val_0, next_digit => nD0);  --ones
+--    dig_1: place_value_driver port map(button => nD0, value => hex_val_1, next_digit => nD1);           --tens
+--    dig_2: place_value_driver port map(button => nD1, value => hex_val_2, next_digit => nD2);           --hundreds
+--    dig_3: place_value_driver port map(button => nD2, value => hex_val_3, next_digit => nD3);           --thousands
+    --compoent call for displaying the data
+    display: sev_seg_driver port map(data_0 => thousands, data_1 => hundreds, data_2 => tens, data_3 => ones, 
+                                     clk => clk, rst => rst, display_data => score_segment, display_digit => score_anode); 
+end Behavioral;
---
+++ Joystick/username_select.vhd
@@ -0,0 +1,212 @@
+--
+-- Company: Point Loma nazarene University
+-- Engineer: Kyle Dramov
+-- 
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+
+entity username_select is
+Port ( 
+    --Pins of the tactile buttons on breadboard
+    right,left, up, down, clk, rst : in STD_LOGIC;
+    --LED's to ensure buttons are working correctly
+    led_right, led_left, led_up, led_down, user_name_out : out STD_LOGIC;
+    -- Anode: Controls which segment is active at any given time
+    name_anode : out std_logic_vector (3 downto 0):= "1111";
+    
+    name_segment : out std_logic_vector (7 downto 0)
+    
+ );
+end username_select;
+
+architecture Behavioral of username_select is
+--Variable that stores the hexvalue to be displayed on each screen.
+signal data_0_i : integer range -2 to 17:= 0; --stores hex value with small buffer on either side
+signal data_1_i : integer range -2 to 17:= 0; --stores hex value with small buffer on either side
+signal data_2_i : integer range -2 to 17:= 0; --stores hex value with small buffer on either side
+
+--Data values to make light Flash
+signal data_0_i_f : integer range -2 to 17:= 16;
+signal data_1_i_f : integer range -2 to 17:= 16;
+signal data_2_i_f : integer range -2 to 17:= 16;
+
+
+--Variables to allow for the user name to be chosen
+signal userName_i: STD_LOGIC := '1';
+signal digitSelector : integer range 0 to 4:= 0;
+
+--Signals to hold previous button press
+signal u: STD_LOGIC:='1';
+signal d: STD_LOGIC:='1';
+
+--Signals for the LED selector indicators
+signal LED0s: STD_LOGIC:='0';
+signal LED1s: STD_LOGIC:='0';
+signal LED2s: STD_LOGIC:='0';
+
+signal count: integer:=0;
+
+component sev_Seg_Driver is
+  port (
+  data_0 : in integer range 0 to 17;
+  data_1 : in integer range 0 to 17;
+  data_2 : in integer range 0 to 17;
+  data_3 : in integer range 0 to 17;
+  clk : in STD_LOGIC;
+  rst : in STD_LOGIC:='0';
+  display_data : out STD_LOGIC_VECTOR(7 downto 0);
+  display_digit : out STD_LOGIC_VECTOR(3 downto 0));
+end component;
+
+component ButtonDebouncer is 
+Port ( Button : in STD_LOGIC;
+       clk : in STD_LOGIC;
+       DebouncedButton : out STD_LOGIC);
+end component;
+
+begin
+   --Making rButton change the digit selector when userName is being edited
+   process
+   begin
+       if userName_i= '1' then
+          if (falling_edge(right)) then
+            digitSelector <= digitSelector + 1;
+            if digitSelector= 4 then
+               digitSelector<= 3;   
+            end if;
+          end if;
+       end if;
+    end process;
+    
+process
+begin
+    if(rising_edge(clk)) then
+        if digitSelector=0 then
+            data_1_i_f<=data_1_i;
+            data_2_i_f<=data_2_i;
+            if(count<30000000) then
+                count<=count+1;
+            else
+                if( data_0_i_f= 16) then
+                     data_0_i_f<=data_0_i;
+                else
+                     data_0_i_f<=16;
+                end if;
+                count<=1;
+            end if;
+
+        elsif(digitSelector=1) then
+            data_0_i_f<=data_0_i;
+            data_2_i_f<=data_2_i;
+           if(count<30000000) then
+                count<=count+1;
+            else
+                if( data_1_i_f =16) then
+                    data_1_i_f<=data_1_i;
+                else
+                    data_1_i_f<=16;
+                end if;
+                count<=1;
+            end if;
+        elsif(digitSelector=2) then
+           data_0_i_f<=data_0_i;
+           data_1_i_f<=data_1_i;
+           if(count<30000000) then
+                count<=count+1;
+            else
+                if(data_2_i_f =16) then
+                    data_2_i_f<=data_2_i;
+                else
+                    data_2_i_f<=16;
+                end if;
+                count<=1;
+            end if;
+        else
+            data_0_i_f<=data_0_i;
+            data_1_i_f<=data_1_i;
+            data_2_i_f<=data_2_i;
+       end if;
+     end if;
+end process;
+    
+    --Process to add or subtract 1 when a button is pressed  
+    process 
+    begin
+       --If user name editing is active
+       if userName_i= '1' then
+         if(rising_edge(clk)) then
+         if digitSelector=0 then
+                -- Button 0 adds 1 when pressed and does nothing when not pressed
+                if (up='0' and u='1' and down='1') then
+                    data_0_i<= data_0_i + 1;
+                elsif (down='0' and d='1' and up='1')then
+                    data_0_i<= data_0_i - 1;
+                else
+                --nothing please
+                end if;
+                u<=up;
+                d<=down;
+                if data_0_i < 0 then 
+                    data_0_i <= 15;
+                elsif data_0_i > 15 then 
+                    data_0_i <= 0;
+                end if;
+         elsif digitSelector=1 then
+                -- Button 0 adds 1 when pressed and does nothing when not pressed
+                if (up='0' and u='1' and down='1') then
+                    data_1_i<= data_1_i + 1;
+                elsif (down='0' and d='1' and up='1')then
+                    data_1_i<= data_1_i - 1;
+                else
+                --nothing please
+                end if;
+                u<=up;
+                d<=down;
+                if data_1_i < 0 then 
+                    data_1_i <= 15;
+                elsif data_1_i > 15 then 
+                    data_1_i <= 0;
+                end if;
+         elsif digitSelector=2 then
+                -- Button 0 adds 1 when pressed and does nothing when not pressed
+                if (up='0' and u='1' and down='1') then
+                    data_2_i<= data_2_i + 1;
+                elsif (down='0' and d='1' and up='1')then
+                    data_2_i<= data_2_i - 1;
+                else
+                --nothing please
+                end if;
+                u<=up;
+                d<=down;
+                if data_2_i < 0 then 
+                    data_2_i <= 15;
+                elsif data_2_i > 15 then 
+                    data_2_i <= 0;
+                end if;
+         elsif digitselector = 3 then
+            userName_i <='0'; 
+         end if;
+         end if;
+       --making nothing happen once userName = 0
+       else
+         led_right <= NOT(right);
+         led_left <= NOT(left);
+         led_up <= NOT(up);
+         led_down <= NOT(down);
+       end if ;
+    end process;
+    
+    --sending the user name to top module
+    user_Name_out<=userName_i;
+    --controls right side LEDs on boolean board, displays username
+    c1: sev_seg_driver port map(data_0 => data_0_i_f, 
+                                data_1 => data_1_i_f, 
+                                data_2 => data_2_i_f, 
+                                data_3 => 16,--blank 
+                                clk => clk, 
+                                rst => rst, 
+                                display_data => name_segment, 
+                                display_digit => name_anode);
+end Behavioral;
